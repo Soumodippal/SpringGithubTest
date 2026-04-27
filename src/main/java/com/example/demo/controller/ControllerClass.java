94448d29150ecd6334db4825a40efa68e2182b5b
Soumodippal <soumodippal2021@gmail.com>
2026-04-27 10:04:07 +0530

    first commit

---
+++ src/main/java/com/example/demo/controller/ControllerClass.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class ControllerClass {
+
+	@GetMapping
+	public ResponseEntity<String> testMethod() {
+		return new ResponseEntity<>("Soumodip pal",HttpStatus.OK);
+	}
+}
